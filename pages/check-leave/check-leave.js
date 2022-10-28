95074efb05578dee28e1ee1556bae5a3cbd9b6a3
plus7c <1959512279@qq.com>
2022-10-28 11:25:06 +0800

    谦

--- pages/check-leave/check-leave.js
+++ pages/check-leave/check-leave.js
@@ -1,4 +1,8 @@
 // pages/leavecheck/leavecheck.js
+
+const db = wx.cloud.database();
+var that = this;
+const app= getApp().appData;
 Component({
   /**
    * 组件的属性列表
@@ -11,13 +15,23 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    leaveList:[],
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+
+    onLoad:function(options) {
+      this.setData({
+        name:app.name,
+        class:app.class,
+        academy:app.academy,
+        
+      })
+    },
+  
 
   }
 })
