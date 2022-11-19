08e82257b02ab8382183cb1558e603aaf3c21eaa
plus7c <1959512279@qq.com>
2022-11-19 18:39:09 +0800

    qqq

--- pages/leave/leave.js
+++ pages/leave/leave.js
@@ -175,7 +175,7 @@ Page({
   bindRegionChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
-      region: 'e.detail.value'
+      region: e.detail.value
     })
   },
 
